35e998c28e612f1a6ff17487b783c729ddcb0b6c
Xavi Moll <xmollv@gmail.com>
2016-08-19 00:25:29 +0200

    Added the class to perform the requests

---
+++ Photorama/Photorama/PhotoStore.swift
@@ -0,0 +1,36 @@
+//
+//  PhotoStore.swift
+//  Photorama
+//
+//  Created by Xavi Moll on 19/08/16.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import Foundation
+
+class PhotoStore {
+    
+    let session: NSURLSession = {
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        return NSURLSession(configuration: config)
+    }()
+    
+    func fetchRecentPhotos() {
+        let url = FlickerAPI.recentPhotosURL()
+        let request = NSURLRequest(URL: url)
+        let task = session.dataTaskWithRequest(request) {
+            (data, response, error) -> Void in
+            
+            if let jsonData = data {
+                if let jsonString = NSString(data: jsonData, encoding: NSUTF8StringEncoding) {
+                    print(jsonString)
+                }
+            } else if let requestError = error {
+                print("Error fetching recent photos: \(requestError)")
+            } else {
+                print("Unexpected error with the request")
+            }
+        }
+        task.resume()
+    }
+}
