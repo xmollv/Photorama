1435b6de40c87e4eceac2774244908c468e78f4b
Xavi Moll <xmollv@gmail.com>
2016-08-18 23:06:36 +0200

    Added the base layout for the project

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Xavi Moll Villalonga on 18/08/16.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
