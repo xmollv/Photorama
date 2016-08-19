254a56e97412f5419efdb732b6a26b741bb9acf3
Xavi Moll <xmollv@gmail.com>
2016-08-19 23:03:36 +0200

    Starting with Core Data. Added the Photo model and created the NSManagedObject subclass

--- Photorama/Photorama/Photo.swift
+++ Photorama/Photorama/Photo.swift
@@ -7,27 +7,24 @@
 //
 
 import UIKit
+import CoreData
 
-class Photo {
+
+class Photo: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
     
-    let title: String
-    let remoteURL: NSURL
-    let photoID: String
-    let dateTaken: NSDate
     var image: UIImage?
     
-    init(title: String, photoID: String, remoteURL: NSURL, dateTaken: NSDate) {
-        self.title = title
-        self.photoID = photoID
-        self.remoteURL = remoteURL
-        self.dateTaken = dateTaken
+    override func awakeFromInsert() {
+        super.awakeFromInsert()
+        
+        // Give the properties their initial values
+        title = ""
+        photoID = ""
+        remoteURL = NSURL()
+        photoKey = NSUUID().UUIDString
+        dateTaken = NSDate()
     }
-    
-}
 
-extension Photo: Equatable {}
-
-func == (lhs: Photo, rhs: Photo) -> Bool {
-    // Two photos are the same if they have the same photoID
-    return lhs.photoID == rhs.photoID
 }
