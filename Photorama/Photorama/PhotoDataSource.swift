a74219de413029f34675a028ee5d87f4d03c2542
Xavi Moll <xmollv@gmail.com>
2016-08-19 21:44:41 +0200

    Created the dataSource for the collectionView

---
+++ Photorama/Photorama/PhotoDataSource.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Xavi Moll on 19/08/16.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+    
+    var photos = [Photo]()
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let identifier = "UICollectionViewCell"
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(identifier, forIndexPath: indexPath)
+        return cell
+    }
+}
