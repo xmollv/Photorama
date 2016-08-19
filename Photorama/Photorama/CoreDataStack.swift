39d8f55ad09f87aff6f74205607e26ef93a351ba
Xavi Moll <xmollv@gmail.com>
2016-08-19 23:18:20 +0200

    Created the Core Data stack

---
+++ Photorama/Photorama/CoreDataStack.swift
@@ -0,0 +1,44 @@
+//
+//  CoreDataStack.swift
+//  Photorama
+//
+//  Created by Xavi Moll on 19/08/16.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    
+    let managedObjectModelName: String
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+       let modelURL = NSBundle.mainBundle().URLForResource(self.managedObjectModelName, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    private var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls.first!
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        var coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let pathComponent = "\(self.managedObjectModelName).sqlite"
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(pathComponent)
+        let store = try! coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        return coordinator
+    }()
+    
+    lazy var mainQueueContext: NSManagedObjectContext = {
+       let moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        moc.persistentStoreCoordinator = self.persistentStoreCoordinator
+        moc.name = "Main Queue Context (UI Context)"
+        return moc
+    }()
+    
+    required init(modelName: String) {
+        managedObjectModelName = modelName
+    }
+}
