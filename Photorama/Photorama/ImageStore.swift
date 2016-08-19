7ebc4d39fef9fe10b90df969b9cbae02d0a07303
Xavi Moll <xmollv@gmail.com>
2016-08-20 00:03:24 +0200

    Storing the images on the disk. Now everything is stored between Core Data and the Filesystem

--- Photorama/Photorama/ImageStore.swift
+++ Photorama/Photorama/ImageStore.swift
@@ -6,4 +6,55 @@
 //  Copyright © 2016 Xavi Moll. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class ImageStore {
+    
+    let cache = NSCache()
+    
+    func setImage(image: UIImage, forKey key: String) {
+        cache.setObject(image, forKey: key)
+        
+        let imageURL = imageURLForKey(key)
+        
+        //TODO: always a jpeg even when adding photos manually?
+        if let data = UIImageJPEGRepresentation(image, 0.5) {
+            data.writeToURL(imageURL, atomically: true)
+        }
+    }
+    
+    func imageForKey(key: String) -> UIImage? {
+        
+        if let existingImage = cache.objectForKey(key) as? UIImage {
+            return existingImage
+        }
+        
+        let imageURL = imageURLForKey(key)
+        
+        guard let imageFromDisk = UIImage(contentsOfFile: imageURL.path!) else {
+            return nil
+        }
+        
+        cache.setObject(imageFromDisk, forKey: key)
+        
+        return imageFromDisk
+    }
+    
+    func deleteImageForKey(key: String) {
+        cache.removeObjectForKey(key)
+        
+        let imageURL = imageURLForKey(key)
+        do {
+            try NSFileManager.defaultManager().removeItemAtURL(imageURL)
+        } catch {
+            print("Error removing images from disk: \(error)")
+        }
+    }
+    
+    func imageURLForKey(key: String) -> NSURL {
+        let documentDirectories = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        let documentDirectory = documentDirectories.first!
+        
+        return documentDirectory.URLByAppendingPathComponent(key)
+    }
+}
--- Photorama/Photorama/PhotoStore.swift
+++ Photorama/Photorama/PhotoStore.swift
@@ -21,6 +21,7 @@ enum PhotoError: ErrorType {
 class PhotoStore {
     
     let coreDataStack = CoreDataStack(modelName: "Photorama")
+    let imageStore = ImageStore()
     
     let session: NSURLSession = {
         let config = NSURLSessionConfiguration.defaultSessionConfiguration()
@@ -73,7 +74,10 @@ class PhotoStore {
     
     func fetchImageForPhoto(photo: Photo, completion: (ImageResult) -> Void) {
         
-        if let image = photo.image {
+        let photoKey = photo.photoKey
+        
+        if let image = imageStore.imageForKey(photoKey) {
+            photo.image = image
             completion(.Success(image))
             return
         }
@@ -88,6 +92,7 @@ class PhotoStore {
             
             if case let .Success(image) = result {
                 photo.image = image
+                self.imageStore.setImage(image, forKey: photoKey)
             }
             
             completion(result)
