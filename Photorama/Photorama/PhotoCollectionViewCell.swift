dba9124da1ffeaf5e266332c651e5e91b7b8fb8c
Xavi Moll <xmollv@gmail.com>
2016-08-19 21:59:35 +0200

    Created the UICollectionViewCell subclass to display the images. We added an activity indicator that will be displayed while the image is downloaded

---
+++ Photorama/Photorama/PhotoCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  File.swift
+//  Photorama
+//
+//  Created by Xavi Moll on 19/08/16.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        updateWithImage(nil)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        updateWithImage(nil)
+    }
+    
+    func updateWithImage(image: UIImage?) {
+        if let imageToDisplay = image {
+            spinner.stopAnimating()
+            imageView.image = imageToDisplay
+        } else {
+            spinner.startAnimating()
+            imageView.image = nil
+        }
+    }
+}
