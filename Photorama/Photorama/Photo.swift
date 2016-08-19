2436b866fc9a9f2b13b20e2f094b5d287c2f5915
Xavi Moll <xmollv@gmail.com>
2016-08-19 21:22:03 +0200

    Added a property to the Photo object to hold the reference to the UIImage

--- Photorama/Photorama/Photo.swift
+++ Photorama/Photorama/Photo.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Xavi Moll. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class Photo {
     
@@ -14,6 +14,7 @@ class Photo {
     let remoteURL: NSURL
     let photoID: String
     let dateTaken: NSDate
+    var image: UIImage?
     
     init(title: String, photoID: String, remoteURL: NSURL, dateTaken: NSDate) {
         self.title = title
