7b570b731583b75737a01aae26efbed0b1a82689
Xavi Moll <xmollv@gmail.com>
2016-08-20 15:08:15 +0200

    Created the viewController for the Tags and the dataSource for the tableView

---
+++ Photorama/Photorama/TagsDataSource.swift
@@ -0,0 +1,29 @@
+//
+//  TagsDataSource.swift
+//  Photorama
+//
+//  Created by Xavi Moll on 20/08/16.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TagsDataSource: NSObject, UITableViewDataSource {
+    
+    var tags: [NSManagedObject] = []
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tags.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("UITableViewCell", forIndexPath: indexPath)
+        
+        let tag = tags[indexPath.row]
+        let name = tag.valueForKey("name") as! String
+        cell.textLabel?.text = name
+        
+        return cell
+    }
+}
---
+++ Photorama/Photorama/TagsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  Photorama
+//
+//  Created by Xavi Moll on 20/08/16.
+//  Copyright © 2016 Xavi Moll. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TagsViewController: UIViewController {
+    
+    var store: PhotoStore!
+    var photo: Photo!
+    
+    var selectedIndexPaths = [NSIndexPath]()
+    
+}
